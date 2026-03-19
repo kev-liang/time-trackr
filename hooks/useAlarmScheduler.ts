326eab9690380227c227b3bf929d0e4c989acb9a
Micheal Yao <kevinman003@gmail.com>
2026-03-18 21:50:08 -0700

    Set alarm when enabling and changing hours

--- hooks/useAlarmScheduler.ts
+++ hooks/useAlarmScheduler.ts
@@ -4,10 +4,13 @@ import { scheduleNotifications } from "@/lib/notifications";
 import { useAlarmStore } from "@/stores/useAlarmStore";
 
 export function useAlarmScheduler() {
+  const enabled = useAlarmStore((s) => s.enabled);
+  const mutedUntil = useAlarmStore((s) => s.mutedUntil);
+  const frequency = useAlarmStore((s) => s.frequency);
+  const frequencyUnit = useAlarmStore((s) => s.frequencyUnit);
+  const schedule = useAlarmStore((s) => s.schedule);
+
   useEffect(() => {
-    const unsub = useAlarmStore.subscribe((state) => {
-      scheduleNotifications(state);
-    });
-    return unsub;
-  }, []);
+    scheduleNotifications({ enabled, mutedUntil, frequency, frequencyUnit, schedule });
+  }, [enabled, mutedUntil, frequency, frequencyUnit, schedule]);
 }
